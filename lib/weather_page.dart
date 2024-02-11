469bc3c47e11585646cf9fd86a2ea5ac6c75402c
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-12 00:17:05 +0530

    Building

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -13,42 +15,65 @@ class WeatherScreen extends StatelessWidget {
             actions: [
           IconButton(
             onPressed: () {}, icon: const Icon(Icons.refresh)
-        )]
+        )] 
      ),
-     body:const Padding(
-       padding: EdgeInsets.all(16.0),
-       child: Column(
-        children: [
-          SizedBox(
-          width: double.infinity,
-          child: Card(
-            elevation:1,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
-            shadowColor: Colors.blueGrey,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column( 
-                children:[Text("300°K", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
-                SizedBox(height: 16,), Icon(Icons.cloud, size: 64,),SizedBox(height: 16,), Text("Rain",style: TextStyle(fontSize: 20),)], 
+     body:ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(15)),
+       child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+         child: const Padding(
+           padding: EdgeInsets.all(16.0),
+           child: Column(
+            children: [
+              SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation:1,
+                shadowColor: Colors.grey ,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column( 
+                    children:[Text("300°K", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 16,), Icon(Icons.cloud, size: 64,),SizedBox(height: 16,), Text("Rain",style: TextStyle(fontSize: 20),)], 
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        SizedBox(height: 15,),
-       
-       //                                      Weather Forecast Cards
-       
-        Text("Weather Forecast", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),),
-       
-       //                                      Additional Info Card
-       
-         Placeholder(
-          fallbackHeight: 150, 
-        ),SizedBox(height: 15,),
-        Text("Additional Information", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),),
-         Placeholder(
-          fallbackHeight: 150, 
-        )],  
+            SizedBox(height: 20,),
+           
+           //                                      Weather Forecast Cards
+           
+            Align(alignment:Alignment.centerLeft,
+             child: Text("Weather Forecast", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22,),)),
+           SizedBox(height: 20,),
+           Row(
+            children: [
+              SizedBox(
+                width: 100,
+                child: Card(
+                  child: Column(
+                    children: [
+                      Text("09:00",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                ),
+              )
+            ]
+           ),
+           //                                      Additional Info Card
+           
+             Placeholder(
+              fallbackHeight: 150, 
+            ),SizedBox(height: 20,),
+            Align(alignment:Alignment.centerLeft,
+            ),
+            SizedBox(height: 20,),
+             Placeholder(
+              fallbackHeight: 150, 
+            )],  
+           ),
+         ),
        ),
      ),
     );
