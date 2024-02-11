109564851cd8d8cc1ccdd6dee1bbfb95bc5bd040
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-11 15:24:07 +0530

    Building

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -5,8 +5,8 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      appBar: AppBar(title: const Text("Weather App", style: TextStyle(fontWeight:FontWeight.bold)),),
     );
   }
 }
