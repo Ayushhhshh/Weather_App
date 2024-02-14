51d21879f43a88eb5c7833d9cbc70a3c7984ffcb
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-14 23:39:35 +0530

    Building

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -15,13 +15,6 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  double temp = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    getCurrentWeather();
-  }
 
 //                                                      API
 
@@ -37,9 +30,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
         throw 'An unexpected error occured'; 
       }
 
-      setState(() {
-        temp = data['list'][0]['main']['temp'];
-      });   
+      return data;
+        
       } catch (e) { 
         throw e.toString();
     }
@@ -59,99 +51,106 @@ class _WeatherScreenState extends State<WeatherScreen> {
         )] 
      ),
      
-     body:ClipRRect(
-      borderRadius:  const BorderRadius.all(Radius.circular(15)),
-       child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-         child:   Padding(
-           padding: const EdgeInsets.all(16.0),
-           child: Column(
-            children: [
-              SizedBox(
-              width: double.infinity,
-              child: Card(
-                elevation:1,
-                shadowColor: Colors.grey ,
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column( 
-                    children:[Text('$temp K', style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 16,), const Icon(Icons.cloud, size: 64,),const SizedBox(height: 16,), const Text("Rain",style: TextStyle(fontSize: 20),)], 
+     body: FutureBuilder(
+      future: getCurrentWeather(),
+       builder: (context , snapshot) {
+        print(snapshot);
+        print(snapshot.connectionState);
+         return ClipRRect(
+        borderRadius:  const BorderRadius.all(Radius.circular(15)),
+         child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+           child:   Padding(
+             padding: const EdgeInsets.all(16.0),
+             child: Column(
+              children: [
+                SizedBox(
+                width: double.infinity,
+                child: Card(
+                  elevation:1,
+                  shadowColor: Colors.grey ,
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column( 
+                      children:[Text(' 200 K', style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+                      const SizedBox(height: 16,), const Icon(Icons.cloud, size: 64,),const SizedBox(height: 16,), const Text("Rain",style: TextStyle(fontSize: 20),)], 
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20,),
-           
-           //                                      Weather Forecast Cards
-           
-            const Align(alignment:Alignment.centerLeft,
-             child: Text("Weather Forecast", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22,),)),
-           const SizedBox(height: 16,),
-           const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-             child: Row(
-              children: [
-             HourlyForecast(
-              time: "09:00",
-              icon: Icons.cloud,
-              temp: "320.16",
+              const SizedBox(height: 20,),
+             
+             //                                      Weather Forecast Cards
+             
+              const Align(alignment:Alignment.centerLeft,
+               child: Text("Weather Forecast", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22,),)),
+             const SizedBox(height: 16,),
+             const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+               child: Row(
+                children: [
+               HourlyForecast(
+                time: "09:00",
+                icon: Icons.cloud,
+                temp: "320.16",
+               ),
+                  HourlyForecast(
+                     time: "12:00",
+                icon: Icons.cloud,
+                temp: "325.52",
+                  ),
+                  HourlyForecast(
+                     time: "15:00",
+                icon: Icons.sunny,
+                temp: "340.06",
+                  ),
+                  HourlyForecast(
+                     time: "18:00",
+                icon: Icons.cloud,
+                temp: "333.22",
+                  ),
+                  HourlyForecast(
+                     time: "21:00",
+                icon: Icons.nights_stay,
+                temp: "312.23",
+                  )
+                ]
+               ),
              ),
-                HourlyForecast(
-                   time: "12:00",
-              icon: Icons.cloud,
-              temp: "325.52",
+             //                                      Additional Info Card
+              const SizedBox(height: 20,),
+              const Align(alignment: Alignment.centerLeft,
+                child: Text("Additional Information",
+                style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
                 ),
-                HourlyForecast(
-                   time: "15:00",
-              icon: Icons.sunny,
-              temp: "340.06",
-                ),
-                HourlyForecast(
-                   time: "18:00",
-              icon: Icons.cloud,
-              temp: "333.22",
-                ),
-                HourlyForecast(
-                   time: "21:00",
-              icon: Icons.nights_stay,
-              temp: "312.23",
-                )
-              ]
-             ),
-           ),
-           //                                      Additional Info Card
-            const SizedBox(height: 20,),
-            const Align(alignment: Alignment.centerLeft,
-              child: Text("Additional Information",
-              style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height:20),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround, 
-               children: [
-                 AdditionalInfo(
-                  icon: Icons.cloud,
-                  label: "Humidity",
-                  value: "94",
-                 ),
-                 AdditionalInfo(
-                   icon: Icons.air,
-                  label: "Wind Speed",
-                  value: "7.67",
-                 ),
-                 AdditionalInfo(
-                   icon: Icons.beach_access,
-                  label: "Pressure",
-                  value: "1000",
-                 ),
-               ],
-             )],  
+              const SizedBox(height:20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, 
+                 children: [
+                   AdditionalInfo(
+                    icon: Icons.cloud,
+                    label: "Humidity",
+                    value: "94",
+                   ),
+                   AdditionalInfo(
+                     icon: Icons.air,
+                    label: "Wind Speed",
+                    value: "7.67",
+                   ),
+                   AdditionalInfo(
+                     icon: Icons.beach_access,
+                    label: "Pressure",
+                    value: "1000",
+                   ),
+                 ],
+               )],  
+             ),
            ),
          ),
-       ),
+       );
+       },
      ),
    );
   }
