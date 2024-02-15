94e4366bce8f6a8bac74ff07c9f500d5a5a7a75c
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-15 23:46:10 +0530

    Building

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -20,7 +20,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   Future<Map<String,dynamic>> getCurrentWeather() async{
     try {
-      String cityName = 'London';
+      String cityName = 'Jaipur';
     final res = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$weatherApiKey')
     );
 
@@ -91,7 +91,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                     padding: const EdgeInsets.all(16.0),
                     child: Column( 
                       children:[Text('$currentTemp K', style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
-                      const SizedBox(height: 16,),  Icon(currentSky == 'Clouds' || currentSky == 'Rain' ? Icons.cloud : Icons.sunny, size: 64,),const SizedBox(height: 16,), Text("$currentSky",style: const TextStyle(fontSize: 20),)], 
+                      const SizedBox(height: 16,),  Icon(currentSky == 'Clouds' || currentSky == 'Rain' || currentSky == 'Clear' ? Icons.cloud : Icons.sunny, size: 64,),const SizedBox(height: 16,), Text("$currentSky",style: const TextStyle(fontSize: 20),)], 
                     ),
                   ),
                 ),
