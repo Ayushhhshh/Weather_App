84c790c063237212b6c6f67d099ab663378c3087
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-02 11:18:54 +0530

    Building

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -21,7 +21,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   Future<Map<String,dynamic>> getCurrentWeather() async{
     try {
-      String cityName = 'Jammu';
+      String cityName = 'Jaipur';
     final res = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$weatherApiKey')
     );
 
